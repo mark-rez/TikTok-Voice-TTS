1e968f6412a94463d49e91db2c87ae40c6f6b9b7
Mark <115313645+GiorDior@users.noreply.github.com>
2023-06-12 15:40:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import argparse
+
+# the script in the directory
+import tiktokvoice
+
+def main():
+    # adding arguments
+    parser = argparse.ArgumentParser(description='TikTok TTS')
+    parser.add_argument('-t', help='text input')
+    parser.add_argument('-v', help='voice selection', choices=tiktokvoice.VOICES)
+    parser.add_argument('-n', help='output filename', default='output.mp3')
+    parser.add_argument('-txt', help='text input from a txt file', type=argparse.FileType('r'))
+    parser.add_argument('-play', help='play sound after generating audio', action='store_true')
+
+    args = parser.parse_args()
+
+    # checking if given values are valid
+    if not args.t and not args.txt:
+        print("Insert a valid text or txt file")
+        return
+
+    if args.t and args.txt:
+        print("Only one input type is possible")
+        return
+    
+    if not args.v:
+        print("No voice has been selected")
+        return
+
+    # executing script
+    if args.t:
+        tiktokvoice.tts(args.t, args.v, args.n, args.play)
+    elif args.txt:
+        tiktokvoice.tts(args.txt.read(), args.v, args.n, args.play)
+
+if __name__ == "__main__":
+    main()
