bbd7ec1f20f953cd0b66a35265e09451ac2bafee
Giorgio <115313645+Giooorgiooo@users.noreply.github.com>
2024-03-26 20:12:22 +0100

    feat: longer text string to demonstrate long audio generation

--- examplescript.py
+++ examplescript.py
@@ -1,7 +1,7 @@
 from tiktokvoice import tts
 
-text = "I am a hero!"
-voice = "en_us_006" # all possible voices can be found here
+text = 'Tangerines are smaller and less rounded than the oranges. The taste is considered less sour, as well as sweeter and stronger, than that of an orange. A ripe tangerine is firm to slightly soft, and pebbly-skinned with no deep grooves, as well as orange in color. The peel is thin, with little bitter white mesocarp. All of these traits are shared by mandarins generally. Peak tangerine season lasts from autumn to spring. Tangerines are most commonly peeled and eaten by hand. The fresh fruit is also used in salads, desserts and main dishes. The peel is used fresh or dried as a spice or zest for baking and drinks. Fresh tangerine juice and frozen juice concentrate are commonly available in the United States.'
+voice = "en_us_006"
 
 # arguments:
 #   - input text
